9e1fdd69a2e5d10632f40b5ee454e8ed3a941f67
Manuel Limones <limonesu.me@gmail.com>
2015-11-22 07:03:34 -0600

    Update keil project

--- mdk/RTE/RTE_Components.h
+++ mdk/RTE/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'Cpp_03_RTOS' 
+ * Project: 'Cpp_06_I2C' 
  * Target:  'Target 1' 
  */
 
