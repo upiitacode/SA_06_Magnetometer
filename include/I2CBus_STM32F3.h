8b9abf2591c1fd1b5565cb1e3d2d03e602c759e1
Manuel Limones <limonesu.me@gmail.com>
2015-11-23 07:56:08 -0600

    Add I2C2 and I2C3 wrappers

--- include/I2CBus_STM32F3.h
+++ include/I2CBus_STM32F3.h
@@ -21,4 +21,40 @@ class I2CBus1 : public I2CBus{
 		virtual void readPacketAt(unsigned char  address_7b, unsigned char regAddress, unsigned char *pData, int nData);
 };
 
+class I2CBus2 : public I2CBus{
+	private:
+		int isBusy(void);
+	public:
+		I2CBus2(void);
+		virtual void writeByte(unsigned char  address_7b, unsigned char data);
+		virtual unsigned char readByte(unsigned char address_7b);
+
+		virtual void writeByteAt(unsigned char  address_7b, unsigned char regAddress, unsigned char data);
+		virtual unsigned char readByteAt(unsigned char address_7b, unsigned char regAddress);
+
+		virtual void writePacket(unsigned char  address_7b, unsigned char  *pData, int nData);
+		virtual void readPacket(unsigned char  address_7b, unsigned char *pData, int nData);
+
+		virtual void writePacketAt(unsigned char  address_7b,unsigned char regAddress, unsigned char  *pData, int nData);
+		virtual void readPacketAt(unsigned char  address_7b, unsigned char regAddress, unsigned char *pData, int nData);
+};
+
+class I2CBus3 : public I2CBus{
+	private:
+		int isBusy(void);
+	public:
+		I2CBus3(void);
+		virtual void writeByte(unsigned char  address_7b, unsigned char data);
+		virtual unsigned char readByte(unsigned char address_7b);
+
+		virtual void writeByteAt(unsigned char  address_7b, unsigned char regAddress, unsigned char data);
+		virtual unsigned char readByteAt(unsigned char address_7b, unsigned char regAddress);
+
+		virtual void writePacket(unsigned char  address_7b, unsigned char  *pData, int nData);
+		virtual void readPacket(unsigned char  address_7b, unsigned char *pData, int nData);
+
+		virtual void writePacketAt(unsigned char  address_7b,unsigned char regAddress, unsigned char  *pData, int nData);
+		virtual void readPacketAt(unsigned char  address_7b, unsigned char regAddress, unsigned char *pData, int nData);
+};
+
 #endif// _I2CBUS_STM32F3_H
--- source/I2CBus_STM32F3.cc
+++ source/I2CBus_STM32F3.cc
@@ -34,6 +34,83 @@ void I2CBus1::readPacket(unsigned char  address_7b, unsigned char *pData, int nD
 void I2CBus1::writePacketAt(unsigned char  address_7b,unsigned char regAddress, unsigned char  *pData, int nData){
 	i2c1_write(address_7b, regAddress, pData, nData);
 }
+
 void I2CBus1::readPacketAt(unsigned char  address_7b, unsigned char regAddress, unsigned char *pData, int nData){
 	i2c1_read(address_7b, regAddress, pData, nData);
+}
+
+I2CBus2::I2CBus2(void){
+	i2c2_init();
+}
+
+void I2CBus2::writeByte(unsigned char  address_7b, unsigned char data){
+	unsigned char txData[1];
+	i2c2_writeRaw(address_7b,DONT_CARE,txData,1,IGNORE_REGISTER_ADDRESS);
+}
+
+unsigned char I2CBus2::readByte(unsigned char address_7b){
+	return 0;
+}
+
+void I2CBus2::writeByteAt(unsigned char  address_7b, unsigned char regAddress, unsigned char data){
+	i2c2_writeRegister(address_7b, regAddress, data);
+}
+
+unsigned char I2CBus2::readByteAt(unsigned char address_7b, unsigned char regAddress){
+	unsigned char rxData[1];
+	i2c2_read(address_7b, regAddress, rxData, 1);
+	return rxData[0];
+}
+
+void I2CBus2::writePacket(unsigned char  address_7b, unsigned char  *pData, int nData){
+	i2c2_writeRaw(address_7b, DONT_CARE, pData, nData,IGNORE_REGISTER_ADDRESS);
+}
+
+void I2CBus2::readPacket(unsigned char  address_7b, unsigned char *pData, int nData){
+}
+
+void I2CBus2::writePacketAt(unsigned char  address_7b,unsigned char regAddress, unsigned char  *pData, int nData){
+	i2c2_write(address_7b, regAddress, pData, nData);
+}
+
+void I2CBus2::readPacketAt(unsigned char  address_7b, unsigned char regAddress, unsigned char *pData, int nData){
+	i2c2_read(address_7b, regAddress, pData, nData);
+}
+
+I2CBus3::I2CBus3(void){
+	i2c3_init();
+}
+
+void I2CBus3::writeByte(unsigned char  address_7b, unsigned char data){
+	unsigned char txData[1];
+	i2c3_writeRaw(address_7b,DONT_CARE,txData,1,IGNORE_REGISTER_ADDRESS);
+}
+
+unsigned char I2CBus3::readByte(unsigned char address_7b){
+	return 0;
+}
+
+void I2CBus3::writeByteAt(unsigned char  address_7b, unsigned char regAddress, unsigned char data){
+	i2c3_writeRegister(address_7b, regAddress, data);
+}
+
+unsigned char I2CBus3::readByteAt(unsigned char address_7b, unsigned char regAddress){
+	unsigned char rxData[1];
+	i2c3_read(address_7b, regAddress, rxData, 1);
+	return rxData[0];
+}
+
+void I2CBus3::writePacket(unsigned char  address_7b, unsigned char  *pData, int nData){
+	i2c3_writeRaw(address_7b, DONT_CARE, pData, nData,IGNORE_REGISTER_ADDRESS);
+}
+
+void I2CBus3::readPacket(unsigned char  address_7b, unsigned char *pData, int nData){
+}
+
+void I2CBus3::writePacketAt(unsigned char  address_7b,unsigned char regAddress, unsigned char  *pData, int nData){
+	i2c3_write(address_7b, regAddress, pData, nData);
+}
+
+void I2CBus3::readPacketAt(unsigned char  address_7b, unsigned char regAddress, unsigned char *pData, int nData){
+	i2c3_read(address_7b, regAddress, pData, nData);
 }
