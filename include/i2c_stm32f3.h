79b10bd04c70694baab3c777f06f451c311a60db
Manuel Limones <limonesu.me@gmail.com>
2015-11-23 05:15:27 -0600

    Add I2C2

--- include/i2c_stm32f3.h
+++ include/i2c_stm32f3.h
@@ -15,6 +15,17 @@ int i2c1_writeRegister(char address7b, char regAddress, char data);
 
 int i2c1_read(char address7b,char regAddress, char* pData, int nData);
 
+void i2c2_init(void);
+
+int i2c2_writeRaw(char address7b,char regAddress, char* pData, int nData, int useRegAddress);
+
+int i2c2_write(char address7b,char regAddress, char* pData, int nData);
+
+int i2c2_writeRegister(char address7b, char regAddress, char data);
+
+int i2c2_read(char address7b,char regAddress, char* pData, int nData);
+
+
 #ifdef __cplusplus
 }
 #endif
--- source/i2c_stm32f3.c
+++ source/i2c_stm32f3.c
@@ -57,7 +57,7 @@ int i2c1_writeRaw(char address7b,char regAddress,char* pData, int nData,int useR
 	// Send start signal
 	I2C_GenerateSTART(I2C1, ENABLE);
 
-	//Send register address 
+	//Send register address
 	if(useRegAddress){
 		while(1){
 			if(I2C_GetFlagStatus(I2C1,I2C_FLAG_NACKF)){// If nack recivied exit with error
@@ -145,5 +145,143 @@ int i2c1_read(char address7b,char regAddress, char* pData, int nData){
 
 	//Send stop signal
 	I2C_GenerateSTOP(I2C1,ENABLE);
+	return I2C_STATUS_SUCCESS;
+}
+
+void i2c2_init(void){
+	// Configure gpio pins for i2c operation PA10:SDA PA9:SCL
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA,ENABLE);
+	GPIO_InitTypeDef myGPIO;
+	GPIO_StructInit(&myGPIO);
+	myGPIO.GPIO_Mode = GPIO_Mode_AF;
+	myGPIO.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_9;
+	myGPIO.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	myGPIO.GPIO_OType = GPIO_OType_OD;
+	GPIO_Init(GPIOA, &myGPIO);
+
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource8, GPIO_AF_4);
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource9, GPIO_AF_4);
+
+	// Configure I2C
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C2, ENABLE);
+	I2C_InitTypeDef myI2C;
+	I2C_StructInit(&myI2C);
+	myI2C.I2C_Ack = I2C_Ack_Enable;
+	myI2C.I2C_Mode = I2C_Mode_I2C;
+	myI2C.I2C_Timing = (PRESC << 28) | (SCLDEL << 20) | (SDADEL << 16) | (SCLH << 8) | (SCLL << 0);//Timing for 100KHz operation
+	I2C_Init(I2C2,&myI2C);
+	I2C_StretchClockCmd(I2C2,ENABLE);
+	I2C_Cmd(I2C2, ENABLE);
+}
+
+int i2c2_write(char address7b, char regAddress, char* pData, int nData){
+	return i2c2_writeRaw(address7b,regAddress,pData,nData,1);
+}
+
+int i2c2_writeRegister(char address7b, char regAddress, char data){
+	char txData[1] = {data};
+	return i2c2_writeRaw(address7b,regAddress,txData,1,1);
+}
+
+int i2c2_writeRaw(char address7b,char regAddress,char* pData, int nData,int useRegAddress){
+	// Set i2c in tramission mode
+	I2C_MasterRequestConfig(I2C2, I2C_Direction_Transmitter);
+	I2C_10BitAddressingModeCmd(I2C2,DISABLE);
+	I2C_SlaveAddressConfig(I2C2,address7b<<1);
+	I2C_NumberOfBytesConfig(I2C2,nData+useRegAddress);
+	I2C_ReloadCmd(I2C2,DISABLE);
+	I2C_AutoEndCmd(I2C2, DISABLE);
+
+	// Send start signal
+	I2C_GenerateSTART(I2C2, ENABLE);
+
+	//Send register address
+	if(useRegAddress){
+		while(1){
+			if(I2C_GetFlagStatus(I2C2,I2C_FLAG_NACKF)){// If nack recivied exit with error
+				return I2C_STATUS_FAIL;
+			}
+			else if(I2C_GetFlagStatus(I2C2,I2C_FLAG_TXE)){
+				I2C_SendData(I2C2,regAddress);
+				break;
+			}
+		}
+	}
+
+	//Send data
+	for(int i = 0; i < nData; i++){
+		while(1){
+			if(I2C_GetFlagStatus(I2C2,I2C_FLAG_NACKF)){// If nack recivied exit with error
+				return I2C_STATUS_FAIL;
+			}
+			else if(I2C_GetFlagStatus(I2C2,I2C_FLAG_TXE)){
+				I2C_SendData(I2C2,pData[i]);
+				break;
+			}
+		}
+	}
+
+	while(!I2C_GetFlagStatus(I2C2,I2C_FLAG_TC));//Wait until trasmission complete
+
+	//Set stop signal
+	I2C_GenerateSTOP(I2C2,ENABLE);
+	return I2C_STATUS_SUCCESS;
+}
+
+int i2c2_read(char address7b,char regAddress, char* pData, int nData){
+	// Wait while busy
+	while(I2C_GetFlagStatus(I2C2,I2C_FLAG_BUSY));
+
+	//Configure bytes transmission
+	I2C_MasterRequestConfig(I2C2, I2C_Direction_Transmitter);
+	I2C_10BitAddressingModeCmd(I2C2,DISABLE);//7 Bit mode
+	I2C_SlaveAddressConfig(I2C2,address7b<<1);
+	I2C_NumberOfBytesConfig(I2C2,1);
+	I2C_ReloadCmd(I2C2,DISABLE);
+	I2C_AutoEndCmd(I2C2, DISABLE);
+
+	// Send start signal
+	I2C_GenerateSTART(I2C2, ENABLE);
+
+	// Wait  until ready to transmittin
+	while(1){
+		if(I2C_GetFlagStatus(I2C2,I2C_FLAG_NACKF)){// If nack return fail
+			return 0;
+		}
+		else if(I2C_GetFlagStatus(I2C2,I2C_FLAG_TXE)){
+			I2C_SendData(I2C2,regAddress);
+			break;
+		}
+	}
+
+	// Wait until transmission done
+	while(!I2C_GetFlagStatus(I2C2,I2C_FLAG_TXE));
+
+	//Change to receiver mode
+	I2C_MasterRequestConfig(I2C2, I2C_Direction_Receiver);
+	I2C_NumberOfBytesConfig(I2C2,nData);
+	I2C_10BitAddressingModeCmd(I2C2,DISABLE);
+
+	// Send restart signal
+	I2C_GenerateSTART(I2C2,ENABLE);
+
+	// Wait until data is recived or nack is detected
+	while(1){
+		if(I2C_GetFlagStatus(I2C2,I2C_FLAG_NACKF)){// Device not found
+			return I2C_STATUS_FAIL;
+		}
+		if(I2C_GetFlagStatus(I2C2,I2C_FLAG_RXNE)){
+			break;
+		}
+	}
+
+	// read data
+	for(int i = 0; i < nData; i++){
+		while(!I2C_GetFlagStatus(I2C2,I2C_FLAG_RXNE));
+			pData[i] = I2C_ReceiveData(I2C2);
+	}
+
+	//Send stop signal
+	I2C_GenerateSTOP(I2C2,ENABLE);
 	return I2C_STATUS_SUCCESS;
 }
